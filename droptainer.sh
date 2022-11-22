2082c2a4b3ad4d19111d92ae7b311fb52f54c1c2
Ivan Markin <git@unkaktus.art>
2022-11-22 19:45:16 +0100

    Export TMPDIR for go-generate permissions

--- droptainer.sh
+++ droptainer.sh
@@ -7,6 +7,8 @@ echo "Extracting apptainer..."
 tar xzf apptainer-1.1.3.tar.gz
 DROPTAINER_ROOT=$PWD
 PATH="$DROPTAINER_ROOT/go/bin/:$PATH"
+# Set TMPDIR so go generate doesn't complain about permissions.
+TMPDIR="$DROPTAINER_ROOT/tmp"
 echo "Building apptainer..."
 cd ./apptainer-1.1.3/
 ./mconfig --without-suid -p $1
@@ -15,5 +17,8 @@ make
 make install
 cd $DROPTAINER_ROOT
 cp squashfuse_ll $1/bin
+echo "[_] Cleaning up..."
+cd
+rm -rf $DROPTAINER_ROOT
 echo "[+] Done!"
 echo "Now, run 'export PATH=\"$1/bin:\$PATH\"' or add it into your .bashrc."
